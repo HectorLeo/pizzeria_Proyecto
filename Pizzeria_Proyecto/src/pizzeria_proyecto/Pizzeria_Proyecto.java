fc768c6d97920a8f4055bba0c6e5c27e41b7e875
snakt <snakt@192.168.1.70>
2017-12-13 21:47:31 -0600

    jframe main

--- Pizzeria_Proyecto/src/pizzeria_proyecto/Pizzeria_Proyecto.java
+++ Pizzeria_Proyecto/src/pizzeria_proyecto/Pizzeria_Proyecto.java
@@ -16,6 +16,9 @@ public class Pizzeria_Proyecto {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        Main miVentana=new Main();
+        miVentana.setVisible(true);
+        miVentana.setEnabled(true);
     }
     
 }
