53abb8376d4ceb5652a0dabce2d5c81d1bdf6ec9
LEÓN <LEN@192.168.1.69>
2017-12-13 21:12:57 -0600

    nuevo

--- Pizzeria_Proyecto/src/Cocinero.java
+++ Pizzeria_Proyecto/src/Cocinero.java
@@ -1,5 +1,9 @@
 public class Cocinero extends Empleado {
 
+    public Cocinero(String Nom_per, String Tel_per, String Direccion_per) {
+        super(Nom_per, Tel_per, Direccion_per);
+    }
+
     public void Cocinar() {
     }
 
