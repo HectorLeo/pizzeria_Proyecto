e6bd599ef67b833436ed31528232beb00e3f6467
snakt <snakt@192.168.1.70>
2017-12-13 21:40:18 -0600

    Metí constructores, bdd

--- Pizzeria_Proyecto/src/Cliente.java
+++ Pizzeria_Proyecto/src/Cliente.java
@@ -1,5 +1,9 @@
 public class Cliente extends Persona {
 
+    public Cliente(String Nom_per, String Tel_per, String Direccion_per) {
+        super(Nom_per, Tel_per, Direccion_per);
+    }
+
     public void Pagar() {
     }
 }
