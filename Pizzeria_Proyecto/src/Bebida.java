0f5807aa1f8224501a085fe8e1ee0b631990ab05
LEÓN <LEN@192.168.1.69>
2017-12-14 13:39:18 -0600

    vientos prro

--- Pizzeria_Proyecto/src/Bebida.java
+++ Pizzeria_Proyecto/src/Bebida.java
@@ -1,4 +1,8 @@
 public class Bebida extends Productos {
 
     private String Sabor;
+
+    public Bebida(String NombrePro, float Precio) {
+        super(NombrePro, Precio);
+    }
 }
--- Pizzeria_Proyecto/src/Papas.java
+++ Pizzeria_Proyecto/src/Papas.java
@@ -1,2 +1,6 @@
 public class Papas extends Productos {
+
+    public Papas(String NombrePro, float Precio) {
+        super(NombrePro, Precio);
+    }
 }
