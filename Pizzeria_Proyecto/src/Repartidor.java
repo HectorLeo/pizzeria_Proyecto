21930c58004718d05d2242486156d026494ed683
LEÓN <LEN@192.168.1.69>
2017-12-13 21:16:10 -0600

    nuevo repartidor

--- Pizzeria_Proyecto/src/Repartidor.java
+++ Pizzeria_Proyecto/src/Repartidor.java
@@ -2,6 +2,18 @@ public class Repartidor extends Empleado {
 
     private String Licencia_conducir;
 
+    public Repartidor(String Nom_per, String Tel_per, String Direccion_per) {
+        super(Nom_per, Tel_per, Direccion_per);
+    }
+     public String getlicencia_conducir(){
+        return Licencia_conducir;
+    
+    }
+     public void setlicencia_conducir(String Licencia_conducir){
+        this.Licencia_conducir=Licencia_conducir;
+    
+    }
+
     public void Entregar_pizza() {
     }
 
