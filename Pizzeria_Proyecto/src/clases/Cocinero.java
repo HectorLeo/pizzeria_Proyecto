6df7ffec64a836f1b986cfefe38ffaff76b27ab9
LEÓN <LEN@192.168.1.69>
2017-12-15 12:34:54 -0600

    ya esta asi

--- Pizzeria_Proyecto/src/clases/Cocinero.java
+++ Pizzeria_Proyecto/src/clases/Cocinero.java
@@ -1,17 +1,31 @@
 package clases;
 
-public class Cocinero extends Empleado {
-
+public class Cocinero extends  Empleado {
+     private boolean continuar;
+    private boolean disponible= true;
     public Cocinero(String Nom_per, String Tel_per, String Direccion_per) {
         super(Nom_per, Tel_per, Direccion_per);
     }
 
     public void Cocinar() {
+        while(disponible == true){
+            try{
+                wait(100);
+                
+            }catch(InterruptedException e){
+                
+            }
+        }
+        //numMjs= msjs;
+        //msj="Mensaje";
+        disponible = true;
+         notifyAll();
     }
 
     public void Empacar() {
     }
 
     public void Notificar_pizza() {
+         continuar= false;
     }
 }
